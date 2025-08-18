04c179c1a4b7367e33ec5b605fde1bfdb51b8d2d
Nayab zahoor <151225222+NayabMalik1@users.noreply.github.com>
2025-08-18 23:17:45 +0500

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -148,6 +148,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nayabzahoor08@gmail.com'
-EMAIL_HOST_PASSWORD = 'diba iaii pqqs qyrm'  # Replace with your actual password
+EMAIL_HOST_USER = 'your gmail'
+EMAIL_HOST_PASSWORD = 'your password'  # Replace with your actual password
 
